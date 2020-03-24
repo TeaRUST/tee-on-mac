139e65119b8d91a34ce0f336c1b0e7fce01cb9c8
jacky.li <jacky.li@accenture.com>
2020-03-24 11:05:21 +0800

    wasi test

--- wasi-test/demo/src/main.rs
+++ wasi-test/demo/src/main.rs
@@ -1,5 +1,8 @@
 use std::env;
+use std::path::PathBuf;
+use std::fs;
 
+#[link(wasm_import_module = "env1")]
 extern "C" {
     fn it_works() -> i32;
     fn open_file() -> i32;
@@ -8,9 +11,48 @@ extern "C" {
 #[no_mangle]
 pub fn start(n: i32) -> i32 {
     let args: Vec<String> = env::args().collect();
-    println!("args : {:?}", env::args());
-    // let _envs: Vec<String> = env::vars().collect();
-    // println!("envs : {:?}", env::vars());
+    println!("args : {:?}", args);
+
+
+    let env_vars = std::env::vars()
+        .map(|(arg, val)| format!("{}={}", arg, val))
+        .collect::<Vec<String>>();
+    println!("env => {:?}", env_vars);
+    let _envs = env::vars();
+    for (key, value) in _envs {
+        println!("env => {:?} = {:?}", key, value);
+    }
+
+    #[cfg(target_os = "wasi")]
+    println!("aaaaaaaaaaaa");
+
+
+    let mut f = fs::OpenOptions::new()
+            .create_new(true)
+            .read(true)
+            .write(true)
+            .open("/xxx")
+            .unwrap();
+    // f.write_all(b"sfdjslfjsldfjsk").unwrap();
+    println!("11111111111");
+
+
+    // let read_dir = std::fs::read_dir("/").unwrap();
+    // let mut out = vec![];
+    // for entry in read_dir {
+    //     out.push(format!("{:?}", entry.unwrap().path()));
+    // }
+    // out.sort();
+
+    // for p in out {
+    //     println!("{}", p);
+    // }
+
+    // let mut base_url = PathBuf::from(".");
+    // base_url.push("x.txt");
+
+    // let x = std::fs::File::open(&base_url).unwrap();
+    // println!("args : {:?}", x);
 
     unsafe { open_file() };
 
--- wasi-test/src/main.rs
+++ wasi-test/src/main.rs
@@ -18,11 +18,12 @@ use std::vec::Vec;
 use wasmer_wasi::{
     is_wasi_module, generate_import_object, get_wasi_version, 
     generate_import_object_for_version,
-    state::{self, WasiFile, WasiFsError},
+    generate_import_object_from_state,
+    state::{self, WasiFile, WasiFsError, WasiState, HostFile},
     types
 };
 
-use std::path::{PathBuf};
+use std::path::{PathBuf, Path};
 
 mod logging;
 
@@ -49,8 +50,8 @@ fn get_module(wasm: &Vec<u8>) -> Module {
     let hash = hasher.encode();
 
     
-    let module = compile_with(&wasm, &compiler).expect("compile error");
-    // let module = compile(&wasm).expect("compile error");
+    // let module = compile_with(&wasm, &compiler).expect("compile error");
+    let module = compile(&wasm).expect("compile error");
   
   
     let f = is_wasi_module(&module);
@@ -67,20 +68,54 @@ pub fn get_instance(wasm: &Vec<u8>) -> Instance {
     let wasi_version = get_wasi_version(&module, false).unwrap();
     println!("get_wasi_version : {:?}", wasi_version);
 
-    let mut base_imports = generate_import_object_for_version(
-        wasi_version, 
-        vec!["jacky".as_bytes().to_vec(), b"hello world".to_vec()], 
-        vec![b"HOME".to_vec()], 
-        vec![
-            PathBuf::from("./asset/")
-        ], 
-        vec![
-            ("file".to_string(), PathBuf::from("./asset/"))
-        ]
-    );
+    // let mut base_imports = generate_import_object(
+    //     // wasi_version, 
+    //     vec!["jacky".as_bytes().to_vec(), b"hello world".to_vec()], 
+    //     vec![b"HOME=1".to_vec()], 
+    //     vec![
+    //         PathBuf::from("asset")
+    //     ], 
+    //     vec![
+    //         ("file".to_string(), PathBuf::from("./asset/"))
+    //     ]
+    // );
+
+    
+    
+    let ws = WasiState::new("wasm")
+        .arg("aaaaaaa")
+        .env("PATH", "sdfljsfdlsfj")  // env still not work, no idea for reason.
+        // .envs({
+        //     let mut hm = std::collections::HashMap::new();
+        //     hm.insert("COLOR_OUTPUT", "TRUE");
+        //     hm.insert("PATH", "/usr/bin");
+        //     hm
+        // })
+        .args(&["jacky", "hello"])
+
+        .preopen(|p| p.directory("asset").read(true).write(true).create(true)).unwrap()
+        // .preopen_dir("asset").expect("preopen_dir")
+        // .preopen(|p| p.directory(cwd).read(true)).expect("preopen")
+        // .map_dir("root", "./asset").expect("map dir")
+        .setup_fs(Box::new(|wfs|{
+            let wasi_file_inner = LoggingWrapper {
+                wasm_module_name: " WASM ".to_string(),
+            };
+
+            wfs
+            .swap_file(types::__WASI_STDOUT_FILENO, Box::new(wasi_file_inner))
+            .unwrap();
+
+            Ok(())
+        }))
+        .build().expect("here");
+
+    // println!("{:?}", ws.fs);
+
+    let mut base_imports = generate_import_object_from_state(ws, wasi_version);
 
     let custom_imports = imports! {
-        "env" => {
+        "env1" => {
             "it_works" => func!(it_works),
             "open_file" => func!(open_file),
         },
@@ -89,18 +124,18 @@ pub fn get_instance(wasm: &Vec<u8>) -> Instance {
     base_imports.extend(custom_imports);
 
     let mut instance = module.instantiate(&base_imports).expect("failed to instantiate wasm module");
-    initialize(instance.context_mut());
+    // initialize(instance.context_mut());
     
     instance
 }
 
 fn main() {
     println!("start");
-    let wasm_binary = std::fs::read("./demo/target/wasm32-wasi/release/demo.wasm").unwrap();
+    let wasm_binary = std::fs::read("./demo/target/wasm32-wasi/debug/demo.wasm").unwrap();
     let instance = get_instance(&wasm_binary);
 
     let entry_point : Func<(i32), i32> = instance.func("start").unwrap();
-    let result = entry_point.call(100).expect("failed to execute plugin");
+    let result = entry_point.call(10).expect("failed to execute plugin");
 
     let gas = metering::get_points_used(&instance);
     println!("wasm result: {} ||| gas: {}", result, gas);
@@ -111,11 +146,27 @@ fn it_works(_ctx: &mut Ctx) -> i32 {
     5
 }
 fn open_file(_ctx: &mut Ctx) -> i32 {
+    println!("aaaaaaa");
     let state = unsafe { state::get_wasi_state(_ctx) };
     // println!("state : {:?}", state);
 
     // let args = String::from_utf8(state.args.as_slice()).unwrap();
-    println!("state : {:?}", &"aa");
+    // println!("state : {:?}", state.fs.open_file_at);
+
+    let read_dir = std::fs::read_dir("./asset/").unwrap();
+    let mut out = vec![];
+    for entry in read_dir {
+        out.push(format!("{:?}", entry.unwrap().path()));
+    }
+    out.sort();
+
+    for p in out {
+        println!("{}", p);
+    }
+
+    let cwd = std::env::current_dir().unwrap();
+    let cwd = cwd.to_str().unwrap();
+    println!("dir => {}", cwd);
 
     1
 }
