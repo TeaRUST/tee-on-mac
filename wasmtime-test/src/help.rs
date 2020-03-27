bde80c21d4e8e791cf4d83e9a8e9529634f83f6d
jacky.li <jacky.li@accenture.com>
2020-03-27 12:15:46 +0800

    update

---
+++ wasmtime-test/src/help.rs
@@ -0,0 +1,9 @@
+use wasmtime::Store;
+use std::fmt;
+
+// impl fmt::Display for Store {
+//   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+
+//     write!(f, "This is a Stroe")
+//   }
+// }
