698596bb8f1409b7643fb1c38d73e0470c53a912
kevingzhang <kevin.zhang.canada@gmail.com>
2020-03-30 13:00:26 -0700

    safely hold and drop buffer memory by using static global

--- binio-wasm/src/lib.rs
+++ binio-wasm/src/lib.rs
@@ -1,15 +1,16 @@
-#![feature(shrink_to)]
 
 use bincode;
 use serde::{Serialize, Deserialize};
-
+static mut BUFFERS : Vec<Vec<u8>> = Vec::new();
 pub fn wasm_prepare_buffer(size: i32) -> i64 {
     let buffer : Vec<u8> = Vec::with_capacity(size as usize);
     let ptr = buffer.as_ptr() as i32;
+    unsafe{BUFFERS.push(buffer)};
     join_i32_to_i64(ptr, size )
 }
 pub fn wasm_deserialize<'a, T>(offset:i32, size:i32)->T where T: Deserialize<'a> {
     let slice = unsafe { std::slice::from_raw_parts(offset as *const u8, size as usize) };
+    let buffer_would_be_dropped = unsafe{BUFFERS.pop()};
     bincode::deserialize(slice).unwrap()
 }
 pub fn join_i32_to_i64( a:i32, b:i32)->i64 {
